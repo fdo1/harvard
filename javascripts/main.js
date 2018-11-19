9c8a50809c0f8c1ed997d001186bebb47e752a97
Fernando <fernando.madser@gmail.com>
2018-11-19 17:05:47 -0600

    Image gallery

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,4 +9,40 @@ $(document).ready(function(){
   $('.close-icon').on('click', function(){
     $('aside').removeClass('shown');
   });
+
+  // Slick initialization
+
+  $('.image-gallery__thumb').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.image-gallery__img-container'
+  });
+
+  $('.image-gallery__img-container').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.image-gallery__thumb',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true,
+    variableWidth: false,
+    adaptiveHeight: false,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 5
+        }
+      }
+    ],
+    prevArrow: $('.prev-button'),
+    nextArrow: $('.next-button')
+  });
+
+  $('.image-gallery__img-container').on('afterChange', function(){
+    $(this).find('img').css('width', '150px');
+  })
 })
